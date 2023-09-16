4f129741f1ac9afe73c0277eb6c72b0470d415af
Elif Alinli <semraelifalinli@gmail.com>
2023-09-16 18:00:50 +0100

    sort category names alphabetically, count display, mechanism to clear categories

--- src/components/CategorySelector/CategorySelector.tsx
+++ src/components/CategorySelector/CategorySelector.tsx
@@ -1,31 +1,52 @@
 import { Category } from "../../types";
+import { Article } from "../../types";
 import "./CategorySelector.css";
+
 
 interface ICategorySelector {
   categories: Category[];
   selectedCategoryId?: Category["id"];
   onSelectCategory: (categoryId: Category["id"]) => void;
+  categoryCounts: Record<string, number>;
+  articles: Article[]
 }
+
+
 
 export function CategorySelector({
   categories,
   selectedCategoryId,
   onSelectCategory,
+  categoryCounts
 }: ICategorySelector) {
+
+
+  const sortedCategories = [...categories].sort((a, b) =>
+  a.title.localeCompare(b.title)
+);
+const handleCategoryClick = (categoryId: string) => {
+  if(selectedCategoryId === categoryId) {
+    onSelectCategory("")
+  } else {
+    onSelectCategory(categoryId)
+  }
+
+}
   return (
     <div className="CategorySelector" data-testid="CategorySelector">
-      {categories.map(({ id, title, color }) => {
+      {sortedCategories.map(({ id, title, color }) => {
         const isSelected = selectedCategoryId === id;
+        const articleCount = categoryCounts[id] || 0;
         return (
           <div
             key={id}
             className="CategoryOption"
             style={{ backgroundColor: isSelected ? "white" : color }}
             onClick={() => {
-              onSelectCategory(id);
+              handleCategoryClick(id);
             }}
           >
-            {title}
+            {title} ({articleCount})
           </div>
         );
       })}
