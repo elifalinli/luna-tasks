97479c8250acc965878b53b4e13d95af9c65cfd2
Elif Alinli <semraelifalinli@gmail.com>
2023-09-20 18:01:02 +0100

    clean-up unnecessary code

--- src/components/ScrollToTop/ScrollToTop.tsx
+++ src/components/ScrollToTop/ScrollToTop.tsx
@@ -1,20 +1,6 @@
 import "./ScrollToTop.css";
-import { useState } from "react";
 
 export function ScrollToTop() {
-    const [visible, setVisible] = useState<boolean>(false)
-
-    const toggleVisible = () => {
-        const scrolled = document.documentElement.scrollTop;
-        if (scrolled > 100){
-          setVisible(true)
-        } 
-        else if (scrolled <= 100){
-          setVisible(false)
-        }
-      };
-
-      window.addEventListener('scroll', toggleVisible);
 
     const scrollToTop = () => {
         window.scrollTo({
@@ -24,7 +10,7 @@ export function ScrollToTop() {
       };
     return (
         <footer>
-            <button className="scroll-top" onClick={scrollToTop} style={{display: visible ? 'inline' : 'none'}}>Scroll to the top 🔝</button>
+            <button className="scroll-top" onClick={scrollToTop}>Scroll to the top 🔝</button>
         </footer>
     )
 }
